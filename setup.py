ed839c0b4dd0d238e10659b50fcf90676454b061
Richard Piccone <contact@infosecsapper.com>
2019-11-11 15:55:13 +0000

    Testing CI dist release automation.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="BitSightAPI",
-    version="0.2.0",
+    version="0.3.1",
     author="InfosecSapper",
     author_email="contact@infosecsapper.com",
     description="A Python wrapper for the BitSight API.",
