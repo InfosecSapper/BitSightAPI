0c8b56117c47a393c556e04a66e4613ab5b42186
Richard Piccone <contact@infosecsapper.com>
2020-03-30 17:54:32 +0100

    Clean up package structure
    Module was incorrectly structured.

--- setup.py
+++ setup.py
@@ -1,18 +1,20 @@
-import setuptools
+from os import system, path
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+setup(
     name="BitSightAPI",
-    version="0.3.1",
+    version="0.3.2",
     author="InfosecSapper",
     author_email="contact@infosecsapper.com",
     description="A Python wrapper for the BitSight API.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/infosecsapper/BitSightAPI",
-    packages=setuptools.find_packages(),
+    license='GPLv3',
+    packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
