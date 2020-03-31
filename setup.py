1cc45fd83cd9649df8c09cb5ec1fef2ae7278c74
Richard Piccone <contact@infosecsapper.com>
2020-03-31 21:19:13 +0100

    Missing comma in setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     packages=find_packages(),
     install_requires=[
         'requests>=2.22.0'
-    ]
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
